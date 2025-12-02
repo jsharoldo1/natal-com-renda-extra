c5eb95b5abf59663218f0a42023dc07e6d252086
José Haroldo Huguenin Câmara Júnior <joseharoldojunior2@gmail.com>
2025-12-02 21:28:45 +0000

    essa subheadline tá muito grande no mobile, precisamos fazer uma versão

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           A Receita de "Panetone Trufado no Pote" que está fazendo donas de casa faturarem de R$ 150 a R$ 400 por dia neste fim de ano
         </h1>
         <p className="text-center text-base md:text-lg text-gray-600 mb-4">
-          Veja o passo a passo de como essa simples adaptação na receita tradicional virou uma febre de vendas e como você pode copiar essa técnica na sua cozinha ainda hoje.
+          Veja o passo a passo que virou febre de vendas e copie essa técnica na sua cozinha ainda hoje.
         </p>
 
         <div className="flex items-center justify-center flex-wrap gap-x-4 gap-y-2 text-gray-600 text-[11px] mb-4">
